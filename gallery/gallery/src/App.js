4f4175bcb4f3ffef329a20ce143deb01037d7735
Quanzer <quanforcollege@gmail.com>
2024-03-03 12:06:30 -0800

    Update for desktop view

--- gallery/gallery/src/App.js
+++ gallery/gallery/src/App.js
@@ -2,10 +2,59 @@ import logo from './logo.svg';
 import './App.css';
 import Gallery from './Gallery';
 import bg from "./img/bg.jpg";
+import {useTypewriter, Cursor} from 'react-simple-typewriter'
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import React, { useState} from 'react';
+
+const style = {
+  background: "#FFFFFF",
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  borderRadius: ".75vh",
+  boxShadow: 24,
+  width: "78%",
+  height: "74%",
+  fontFamily: "DM Sans",
+  outline: "none",
+  border: 'none',
+  
+  
+}
+
 function App() {
+
+  const [open, setOpen] = useState(true);
+  const handleClick = () => {
+    setOpen(false);
+  }
+  const [text] = useTypewriter({
+    words: ['Hey Wynne, I know Im not the best when it comes to putting my feelings into words, but I wanted to share this with you. I hope you like it :) (Click on each photo when ur in).'],
+   
+    typeSpeed: 50,
+    deleteSpeed: 80,
+  });
+  
   return (
     <>
      
+      <Modal
+        open={open}
+        onClose={handleClick}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          
+        <Button style = {{paddingTop: "5%",float: "right", color : "#000000" }} onClick={handleClick}><div>X</div></Button>
+        <div  style = {{padding: "10%"}}>{text}</div>
+        </Box>
+      </Modal>
+    
     <Gallery/>
     
  
--- gallery/gallery/src/Gallery.js
+++ gallery/gallery/src/Gallery.js
@@ -4,6 +4,7 @@ import React, { useState,useEffect} from 'react';
 import { Unstable_Popup as BasePopup } from '@mui/base/Unstable_Popup';
 import { styled } from '@mui/system';
 import './gallery.css'
+import { Slide,  Zoom } from '@mui/material';
 
 import img1 from './img/img1.JPG'
 import img2 from './img/img2.JPG'
@@ -29,28 +30,32 @@ const Gallery = () => {
   const toggleStyle =  (event, index) => {
     switch (index) {
     case 1:
-      setText("I love you");
+      setText("03/28/2023. "+
+      "Where it all started, one of our first dates,  and the one of the first moments I started to realize my love for you :).  I remember thinking that day how did you even end up still like me when I was acting the way I was before I knew you liked me."+
+      "I tried pushing away my feelings for you before becausae I did not know how you felt, but you made it extremely easy to fall for you again with your kindness, love, and beauty 🤭.");
       break;
     case 2:
-      setText("You're my sunshine");
+      setText("07/01/2023. Remember when we were in the car 👀 and your NorCal friends called you and you wanted me to talk to them, then they suddenly wanted to go to VEGAS. This was still about a month before this picture was taken and it was still early but you wanted me to go. I was a little hesitant but I ended up loving my time in Vegas. I remember one of your friends saying 'Idk how YOUR girl got us into this club, shes is awesome' and I thought to myself that you were pretty amazing and how your friends already viewed us as a couple. We were also the only couple to share a single room together, which I thought was really exciting and cool, ty for that :).");
       break;
     case 3:
-      setText("You make my heart skip a beat");
+      setText("08/16/2023. Despite a day tinged with my sneezing, every moment spent with you here feels like a fairytale come true. From scoring discounted tickets through my sister's friend to wandering hand in hand through Disney's magical world, every laugh, every shared glance, and every thrilling ride was a testament to our love's resilience. As we watched the fireworks light up the night sky, your presence made everything feel enchanting. Together, we explored every corner, tried every delectable treat, and dared every exhilarating ride, creating memories that will forever sparkle like the stars above. With you, every moment is a cherished adventure, and I'm endlessly grateful for the magic you bring into my life.");
       break;
     case 4:
-      setText("You complete me");
+      setText("08/26/2023. Our first rave together and when I met your riverside friends for the first time. Amidst the rolling, I shared my heart's desires, asking if I could meet your family, only to be met with: why haven't I asked you out officially yet? 💞 In that instant, surrounded by the raving crowd of SLANDER, I knew the time was right. I never told you why I took so long but I was dealing with my own emotions about if I felt ready or not to commit to a relationship but that night it made me realize that I shouldn't wait any longer. And so, a week later, in the warmth of our shared love, I asked you to be my girlfriend, and the rest was history 🌟.");
       break;
     case 5:
-      setText("You're my everything");
+      setText("09/03/2023. Captured in  lights and delectable aromas of the 626 Night Market, this was the where it all started (again). Together, we navigated through vulnerable conversations, sharing our deepest feelings and desires. And in that moment of shared vulnerability, I realized that love knows no bounds. 🌹 We were talking about our feelings and made a decision together—to delete Hinge, symbolizing our commitment to each other. I asked you to be my girlfriend officially right when I saw you this day, knowing that with you, I have found my home and my heart's truest desire. 💫");
       break;
     case 6:
-      setText("You're the reason for my smile");
+      setText("10/02/2023. This was a magical night, as we ventured back to the city of Riverside for lavender nights and walked around for a few hours. This was one of the least expensive dates yet it is one of my favorites, I loved just walking around and looking at lights with you and eating the overly salted fries that we bought 😆."
+      + "The pictures we took this night were also some of my all time favorites. I found myself captivated not just by the delicate blooms surrounding us, but by the ethereal beauty of the person beside me. 💜 ");
       break;
     case 7:
-      setText("I'm so lucky to have you");
+      setText("01/01/2024. On the last night of Decadence, surrounded by the rave lights, we welcomed the new year with hearts overflowing with love and joy. 🎉 I introduced you for the first time to Vinh and Danny for the first time, and this was a big moment for me as those are two of my closest friends and family members, and I was so happy when they liked you and vice versa. This was also one of my favorite moments of us together as we grew to trust each other more and be honest with each other." 
+      + "I'm so lucky to have you in my life.");
       break;
     case 8:
-      setText("You mean the world to me");
+      setText("01/27/2024. In the dazzling lights of our second trip to Vegas, amidst the hours of walking and burning feet, our bond only grew stronger. 🎲 Sharing the excitement of Sin City with my closest friends, I watched happily as you effortlessly connected with them and their girlfriends. 💖 Seeing you laugh and share moments with my friend group brought me immense joy, knowing that you were not just my partner, but an integral part of my life and friendships. Every moment spent with you is a testament to your beauty, both inside and out, and I am endlessly grateful to have you by my side, my radiant beacon of love and light. 🌟 You mean the world to me and I love you.");
       break;
     default:
       setText("You're special to me");
@@ -97,16 +102,31 @@ const Gallery = () => {
     }
     return () => clearTimeout(timeoutId);
   }, [isZoomed]);
+  const [isHorizontal, setIsHorizontal] = useState(false);
+
+  useEffect(() => {
+    const checkOrientation = () => {
+      setIsHorizontal(window.innerWidth > window.innerHeight);
+    };
+
+    // Initial check
+    checkOrientation();
+
+    // Listen for resize events to update orientation
+    window.addEventListener('resize', checkOrientation);
+
+    // Clean up event listener on component unmount
+    return () => window.removeEventListener('resize', checkOrientation);
+  }, []);
   return (
     <>
    
-    <div style = {{marginLeft :"10vw", marginTop: "-16vh"}}>
-        
-    <div style = {{  marginBottom:"5vh"}}>
-    <img  style = {{ height: "80%", width: "80%"}} className={isZoomed ? 'zoomed' : 'notzoomed'}  src = {HVW} alt = "" /> 
-       
-    
-    </div>
+    <div style = {{marginLeft :isHorizontal ? "10vw": "10vw", marginTop: "-16vh"}}>     
+      <div style = {{ marginLeft :isHorizontal ? "7vw": "4vw",  marginBottom:"5vh"}}>
+        <img  style = {{ height:isHorizontal ? "60%": "80%", width: isHorizontal ? "60%": "80%"}} 
+        className={isZoomed ? 'zoomed' : 'notzoomed'}  
+        src = {HVW} alt = "" /> 
+      </div>
    
     <div style = {{position: "relative",zIndex: 1, marginLeft: "15vw", display: "flex"}} className={currentStyle} >
     <BasePopup id={id} open={open} anchor={anchor}>
@@ -123,18 +143,23 @@ const Gallery = () => {
       
     </div >
   
-    <div style = {{position: "relative", zIndex: 2, bottom:0,marginBottom:"-9vh", marginLeft :"-5vw", marginTop: "30vh",display: "flex"}}>
-   
-    <img onClick={togglePlayback}  className={isZoomed ? 'zoomed' : 'notzoomed'} style = {{ height: "70%", width: "70%",  display: "flex",marginBottom: "5%",marginTop: "-65%",marginLeft :"8vw"}} 
+    <div   style = {{position: "relative", zIndex: 2, bottom:0,marginBottom:"-9vh", marginLeft :"-5vw", marginTop: "30vh",display: "flex"}}>
+       
+    <Slide direction='out' in={currentStyle == 'slider'} mountOnEnter unmountOnExit >
+     
+      <img onClick={togglePlayback}  
+       className= {!isPlaying? 'onScene' : ''} 
+       style = {{ height: isHorizontal ? "40%" : "70%" , width: isHorizontal ? "40%" : "70%",  display: "flex",marginBottom: isHorizontal ? "0%": "-6%" ,marginTop: isHorizontal ? "-26%":"-45%",marginLeft : isHorizontal ?"22%": "8vw"}} 
        src = {isPlaying? cGIF : cStill} alt = "Animated GIF"/>
+       </Slide>
     
     </div>
-    </div>
-    <audio ref={audioRef} loop>
+  </div>
+  <audio ref={audioRef} loop>
         
-        <source src= {skel} type="audio/mpeg" />
+    <source src= {skel} type="audio/mpeg" />
         
-      </audio>
+  </audio>
    
     </>
   );
@@ -163,20 +188,22 @@ const grey = {
   };
   
   const PopupBody = styled('div')(
+    
     ({ theme }) => `
-    width: max-content;
-    padding: 12px 16px;
+    width: ${window.innerWidth > window.innerHeight? '50vw' : '85vw'};
+    
+    padding: 5px 5px;
     margin: 8px;
     border-radius: 8px;
     border: 1px solid ${theme.palette.mode === 'dark' ? grey[700] : grey[200]};
-    background-color: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
+    background-color: ${theme.palette.mode === 'dark' ? grey[900] : '#F5F5DC'};
     box-shadow: ${
       theme.palette.mode === 'dark'
         ? `0px 4px 8px rgb(0 0 0 / 0.7)`
         : `0px 4px 8px rgb(0 0 0 / 0.1)`
     };
     font-family: 'DM Sans', sans-serif;
-    font-size: 0.875rem;
+    font-size: ${window.innerWidth > window.innerHeight? '1vw' : '1vh'};
     z-index: 1;
   `,
   );
